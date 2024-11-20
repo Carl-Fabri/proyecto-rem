916926d456105a3723ff2615c64cf30182874259
Carl-Fabri <ecefabriciof@gmail.com>
2024-11-20 04:23:50 -0500

    feat: add password validation

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,12 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AuthService } from '../core/service/auth.service';
+import { Router } from '@angular/router';
+import { matchPassword } from '../core/validators/match-password.validator';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit{
+  authService = inject(AuthService);
+  _router = inject(Router);
+  fb = inject(FormBuilder);
+
+  hide = true;
+  form! : FormGroup;
+
+  register() {
+    if (this.form.invalid) return;
+    this.authService.register(this.form.value).subscribe({
+      next: (response)=> {
+        this._router.navigateByUrl('hypnoproyecciones');
+      },
+      error: (error)=> {
+        console.log('Login error');
+      }
+    }
+  );
+
+  }
+  ngOnInit(): void {
+    this.form = this.fb.group({
+      name: ['',[Validators.required]],
+      email: ['',[Validators.required,Validators.email]],
+      password: ['',Validators.required],
+      confirmPassword: ['',Validators.required]
+    },{
+      validator: matchPassword
+    });
+
+  }
 
 }
