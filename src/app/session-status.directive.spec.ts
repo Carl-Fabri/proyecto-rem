044281cf2186ca3ec7be9802afb794fdd72e6cec
Carl-Fabri <ecefabriciof@gmail.com>
2024-11-20 04:24:08 -0500

    fix: remove testing archive

--- src/app/session-status.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { SessionStatusDirective } from './session-status.directive';
-
-describe('SessionStatusDirective', () => {
-  it('should create an instance', () => {
-    const directive = new SessionStatusDirective();
-    expect(directive).toBeTruthy();
-  });
-});
